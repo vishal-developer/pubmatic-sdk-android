57b48aa0fea0b8b25b4ad6f189d4c83212ce930b
Udayan Warnekar <udayan.warnekar@pubmatic.com>
2015-09-16 20:43:53 +0530

    Cleanup

--- Adapters/MoPubNativeAdapterSampleApp/src/com/mocean/mopubnativeadapter/sample/NativeAdapterSample.java
+++ Adapters/MoPubNativeAdapterSampleApp/src/com/mocean/mopubnativeadapter/sample/NativeAdapterSample.java
@@ -6,8 +6,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
