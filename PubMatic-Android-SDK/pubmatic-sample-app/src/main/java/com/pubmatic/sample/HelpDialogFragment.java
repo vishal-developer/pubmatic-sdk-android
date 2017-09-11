0089b3a02ad53a30a1bccfa26885eb083bc16ee9
Shrawan Gupta <shrawan.gupta@pubmatic.com>
2017-09-11 18:38:11 +0530

    Updated Help screen text in Sample screen.

--- PubMatic-Android-SDK/pubmatic-sample-app/src/main/java/com/pubmatic/sample/HelpDialogFragment.java
+++ PubMatic-Android-SDK/pubmatic-sample-app/src/main/java/com/pubmatic/sample/HelpDialogFragment.java
@@ -24,12 +24,12 @@ public class HelpDialogFragment extends DialogFragment {
         return "<h2>Welcome to the PubMatic's SDK Sample app.</h2><br/>" +
                 "    This application helps you to test PubMatic's ad tag.<br/>" +
                 "    <br/>How to use:<br/>" +
-                "    1. Please select the desired ad type<br/>" +
+                "    1. Select the desired ad type<br/>" +
                 "    2. Input your ad tag details PubID (Publisher ID), SiteID (Publisher's site ID), AdID (Ad slot ID). Please get these details while creating an ad tag from PubMatic UI or please contact PubMatic sales team to get new ad tags for your app.<br/>" +
                 "    3. Optionally you can configure other targeting parameters for better monetisation.<br/>" +
-                "    4. App also provides Settings screen which allows to apply global settings for all platform and ad type.<br/><br/>" +
+                "    4. App also provides Settings screen which allows to apply global settings.<br/><br/>" +
                 "        <h3>Contact:</h3><br/>" +
-                "    To monetize your app using PubMatic platform, Please <a href='https://pubmatic.com/contact-us/'>contact us</a>";
+                "    To monetize your app using PubMatic platform, Please contact support@pubmatic.com</a>";
     }
 
     @Override
